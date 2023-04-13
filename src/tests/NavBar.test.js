c91aef0768c420e5ebd154cd9bdb9a9cdf7c01da
Matias Aduco <matias.aduco@gmail.com>
2023-04-13 04:10:22 -0300

    Creada y terminada la NavBar con sus respectivos Test.

---
+++ src/tests/NavBar.test.js
@@ -0,0 +1,34 @@
+import { render, screen } from '@testing-library/react';
+import NavBar from '../components/NavBar';
+
+beforeEach(() => {
+  render(<NavBar />);
+});
+
+describe('<NavBar />', () => {
+  test('Existe un tag <nav><nav/>', () => {
+    const nav = screen.getByRole('navigation');
+
+    expect(nav).toBeInTheDocument();
+  });
+
+  test('Existe un tag <ul><ul/>', () => {
+    const ul = screen.getByRole('list');
+
+    expect(ul).toBeInTheDocument();
+  });
+
+  test('No existe <li><li/>', () => {
+    const li = screen.queryByRole('listitem');
+
+    expect(li).toBeFalsy();
+  });
+
+  test('Crear botones <li><li/>', () => {
+    const buttonsList = ['Inicio', 'Manuales', 'Aventuras'];
+    render(<NavBar buttons={buttonsList} />);
+    const li = screen.getAllByRole('listitem');
+
+    expect(li.length).toBe(buttonsList.length);
+  });
+});
