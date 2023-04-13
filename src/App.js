0b04f8562d060bba6a435aae4c7af9e75fcca4c9
Matias Aduco <matias.aduco@gmail.com>
2023-04-13 15:51:49 -0300

    Implementado Router en App.js.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Aventuras from './components/Aventuras/Aventuras';
 import NavBar from './components/NavBar/NavBar';
 
+const router = createBrowserRouter([
+  { path: '/', element: <></> },
+  { path: '/aventuras', element: <Aventuras /> },
+]);
 
 function App() {
   return (
     <>
       <NavBar />
+      <RouterProvider router={router} />
     </>
   );
 }
