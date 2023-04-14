97a0d21e28cc94b6773d8333daba9e4ae8c6fa32
Matias Aduco <matias.aduco@gmail.com>
2023-04-14 00:35:08 -0300

    Se agregaron imágenes a las Aventuras oficiales pre-cargadas.

--- src/components/PDFView.js
+++ src/components/PDFView.js
@@ -1,9 +1,10 @@
 import { useLocation } from 'react-router-dom';
 
-export const AventuraView = () => {
+export const PDFView = () => {
   const location = useLocation();
   const adventure = location.state;
-  const pdf = require(`../../resources/adventures/${adventure.download}`);
+  const pdf = require(`../resources/adventures/${adventure.download}`);
+
   return (
     <center>
       <h1>{adventure.title}</h1>
@@ -11,3 +12,5 @@ export const AventuraView = () => {
     </center>
   );
 };
+
+export default PDFView;
