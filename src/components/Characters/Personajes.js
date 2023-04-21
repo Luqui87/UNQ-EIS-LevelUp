c1603e8c535ed3e790561132b5e01e15e9d69992
Matias Aduco <matias.aduco@gmail.com>
2023-04-21 03:52:21 -0300

    Agregado botón para ver personajes predeterminados. Dejo inhabilitado el botón de ver mis personajes hasta que los podemos crear.

--- src/components/Characters/Personajes.js
+++ src/components/Characters/Personajes.js
@@ -3,16 +3,29 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 import loading_icon from '../../resources/loading.gif';
 import PersonajeCard from './PersonajeCard.js';
+import CharacterNotFound from './CharacterNotFound';
 
 export const Personajes = () => {
-  const [data, setData] = useState([{}]);
+  const [myChars, setMyChars] = useState([{}]);
+  const [defaultCharacters, setDefaultCharacters] = useState([{}]);
   const [loading, setLoading] = useState(true);
+  const [myCharsTab, setMyCharsTab] = useState(false);
+
+  useEffect(() => {
+    fetch('http://localhost:3010/characters/geek', { method: 'GET' })
+      .then((response) => response.json())
+      .then((data) => {
+        setMyChars(data);
+        setLoading(false);
+      })
+      .catch((error) => console.error(error));
+  }, []);
 
   useEffect(() => {
     fetch('http://localhost:3010/characters/default', { method: 'GET' })
       .then((response) => response.json())
       .then((data) => {
-        setData(data);
+        setDefaultCharacters(data);
         setLoading(false);
       })
       .catch((error) => console.error(error));
@@ -27,13 +40,32 @@ export const Personajes = () => {
           style={{ maxWidth: '20em', margin: '0 auto' }}
         />
       ) : (
-        <>
-          <div className='characters'>
-            {data.map((personaje) => {
+        <div className='characters'>
+          {/* <button
+            className={myCharsTab ? 'active' : ''}
+            onClick={() => setMyCharsTab(true)}
+          >
+            Mis Personajes
+          </button> */}
+          <button
+            className={myCharsTab ? '' : 'active'}
+            onClick={() => setMyCharsTab(false)}
+          >
+            Personajes Predeterminados
+          </button>
+          {myCharsTab &&
+            (myChars.length !== 0 ? (
+              myChars.map((personaje) => {
+                return <PersonajeCard personaje={personaje} />;
+              })
+            ) : (
+              <CharacterNotFound />
+            ))}
+          {!myCharsTab &&
+            defaultCharacters.map((personaje) => {
               return <PersonajeCard personaje={personaje} />;
             })}
-          </div>
-        </>
+        </div>
       )}
     </>
   );
