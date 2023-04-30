dfc6401ecff8bc2e90cfb9d8745860b2035cc0b4
Ivana Provvidenza <vanndere@gmail.com>
2023-04-30 16:47:24 -0300

    Cambio API signin para revision

--- src/Api.js
+++ src/Api.js
@@ -1,20 +1,20 @@
-const API_URL = 'http://localhost:3010'
+const API_URL = "http://localhost:3010";
 
 export const getCharacter = (owner, id) => {
-  return fetch(`${API_URL}/characters/${owner}/${id}`, { method: 'GET' })
-    .then(response => response.json())
-    .catch(error => console.error(error))
-}
+  return fetch(`${API_URL}/characters/${owner}/${id}`, { method: "GET" })
+    .then((response) => response.json())
+    .catch((error) => console.error(error));
+};
 
-export const signin = (user) => {
-  fetch(`http://localhost:3010/auth/signin`, {
-    method: 'POST',
+export const signin = async (user) => {
+  return await fetch(`http://localhost:3010/auth/signin`, {
+    method: "POST",
     body: JSON.stringify(user),
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
   })
-    .then(response => console.log(response))
-    .then(data => console.log(data))
-    .catch(error => console.log(error))
-}
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error));
+};
