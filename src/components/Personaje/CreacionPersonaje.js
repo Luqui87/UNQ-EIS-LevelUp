59294c66123c46e2d0c43125c28fb69f869d26ec
Matias Aduco <matias.aduco@gmail.com>
2023-05-05 22:25:44 -0300

    Ya se puede crear un personaje y se guarda en la base de datos.

--- src/components/Personaje/CreacionPersonaje.js
+++ src/components/Personaje/CreacionPersonaje.js
@@ -3,11 +3,51 @@ import races from '../../resources/character/races'
 import classes from '../../resources/character/classes'
 import aligments from '../../resources/character/alignments'
 import './creacion_personaje.css'
+import { useNavigate } from 'react-router-dom'
+import { createCharacter } from '../../Api'
 
 export const CreacionPersonaje = () => {
-  const [alineamiento, setAlineamiento] = useState('')
-  const [raza, setRaza] = useState('')
-  const [clase, setClase] = useState('')
+  const navigate = useNavigate()
+  const [error, setError] = useState('')
+  const [character, setCharacter] = useState({
+    fullname: '',
+    owner: 'geek',
+    race: '',
+    class: '',
+    level: '0',
+    alignment: '',
+    strength: '',
+    dexterity: '',
+    constitution: '',
+    intelligence: '',
+    wisdom: '',
+    charisma: '',
+    biography: '',
+    img: '',
+  })
+
+  const send = async () => {
+    if (!character.fullname)
+      return setError('Se requiere un nombre de personaje válido.')
+    if (!character.level)
+      return setError('Se requiere un nivel de personaje válido, entre 1 y 20.')
+    if (!character.race) return setError('Debe de seleccionar una raza.')
+    if (!character.class) return setError('Debe de seleccionar una clase.')
+    if (!character.alignment)
+      return setError('Debe de seleccionar un alineamiento.')
+    if (
+      !character.strength ||
+      !character.dexterity ||
+      !character.constitution ||
+      !character.intelligence ||
+      !character.wisdom ||
+      !character.charisma
+    )
+      return setError('Es necesario que complete todas las estadísticas.')
+    const res = await createCharacter(character)
+    alert(res.message)
+    navigate('/personajes')
+  }
 
   return (
     <div className='character_sheet'>
@@ -16,6 +56,9 @@ export const CreacionPersonaje = () => {
           type='text'
           placeholder='Nombre del Personaje'
           id='character_name'
+          onChange={event =>
+            setCharacter(char => ({ ...char, fullname: event.target.value }))
+          }
         />
         <input
           type='number'
@@ -23,6 +66,9 @@ export const CreacionPersonaje = () => {
           min='1'
           max='20'
           id='character_level'
+          onChange={event =>
+            setCharacter(char => ({ ...char, level: event.target.value }))
+          }
         />
       </section>
       <section className='alignments'>
@@ -33,10 +79,22 @@ export const CreacionPersonaje = () => {
               <input
                 type='radio'
                 value={alignment.value}
-                checked={alineamiento === alignment.value}
-                onChange={event => setAlineamiento(event.target.value)}
+                checked={character.alignment === alignment.value}
+                onChange={event =>
+                  setCharacter(char => ({
+                    ...char,
+                    alignment: event.target.value,
+                  }))
+                }
               />
-              <label onClick={() => setAlineamiento(alignment.value)}>
+              <label
+                onClick={() =>
+                  setCharacter(char => ({
+                    ...char,
+                    alignment: alignment.value,
+                  }))
+                }
+              >
                 {alignment.name}
               </label>
             </span>
@@ -44,8 +102,9 @@ export const CreacionPersonaje = () => {
         })}
         <div className='description'>
           {
-            aligments.filter(alignment => alignment.value === alineamiento)[0]
-              ?.description
+            aligments.filter(
+              alignment => alignment.value === character.alignment
+            )[0]?.description
           }
         </div>
       </section>
@@ -57,10 +116,24 @@ export const CreacionPersonaje = () => {
               <input
                 type='radio'
                 value={race.value}
-                checked={raza === race.value}
-                onChange={event => setRaza(event.target.value)}
+                checked={character.race === race.value}
+                onChange={event =>
+                  setCharacter(char => ({
+                    ...char,
+                    race: event.target.value,
+                  }))
+                }
               />
-              <label onClick={() => setRaza(race.value)}>{race.name}</label>
+              <label
+                onClick={() =>
+                  setCharacter(char => ({
+                    ...char,
+                    race: race.value,
+                  }))
+                }
+              >
+                {race.name}
+              </label>
             </span>
           )
         })}
@@ -73,18 +146,32 @@ export const CreacionPersonaje = () => {
               <input
                 type='radio'
                 value={c.value}
-                checked={clase === c.value}
-                onChange={event => setClase(event.target.value)}
+                checked={character.class === c.value}
+                onChange={event =>
+                  setCharacter(char => ({
+                    ...char,
+                    class: event.target.value,
+                  }))
+                }
               />
-              <label onClick={() => setClase(c.name)}>{c.name}</label>
+              <label
+                onClick={() =>
+                  setCharacter(char => ({
+                    ...char,
+                    class: c.value,
+                  }))
+                }
+              >
+                {c.name}
+              </label>
             </span>
           )
         })}
         <div className='description'>
-          {classes.filter(c => c.value === clase)[0]?.description}
+          {classes.filter(c => c.value === character.class)[0]?.description}
           <ul>
             {classes
-              .filter(c => c.value === clase)[0]
+              .filter(c => c.value === character.class)[0]
               ?.capabilities.map(cap => (
                 <li>{cap}</li>
               ))}
@@ -94,30 +181,95 @@ export const CreacionPersonaje = () => {
       <section className='stats'>
         <span>
           <label>Fuerza</label>
-          <input type='number' />
+          <input
+            type='number'
+            onChange={event =>
+              setCharacter(char => ({
+                ...char,
+                strength: event.target.value,
+              }))
+            }
+          />
         </span>
         <span>
           <label>Destreza</label>
-          <input type='number' />
+          <input
+            type='number'
+            onChange={event =>
+              setCharacter(char => ({
+                ...char,
+                dexterity: event.target.value,
+              }))
+            }
+          />
         </span>
         <span>
           <label>Constitución</label>
-          <input type='number' />
+          <input
+            type='number'
+            onChange={event =>
+              setCharacter(char => ({
+                ...char,
+                constitution: event.target.value,
+              }))
+            }
+          />
         </span>
         <span>
           <label>Inteligencia</label>
-          <input type='number' />
+          <input
+            type='number'
+            onChange={event =>
+              setCharacter(char => ({
+                ...char,
+                intelligence: event.target.value,
+              }))
+            }
+          />
         </span>
         <span>
           <label>Sabiduría</label>
-          <input type='number' />
+          <input
+            type='number'
+            onChange={event =>
+              setCharacter(char => ({
+                ...char,
+                wisdom: event.target.value,
+              }))
+            }
+          />
         </span>
         <span>
           <label>Carisma</label>
-          <input type='number' />
+          <input
+            type='number'
+            onChange={event =>
+              setCharacter(char => ({
+                ...char,
+                charisma: event.target.value,
+              }))
+            }
+          />
         </span>
       </section>
-      <textarea placeholder='Escriba la historia de su personaje...' />
+      <textarea
+        placeholder='Escriba la historia de su personaje...'
+        onChange={event =>
+          setCharacter(char => ({
+            ...char,
+            biography: event.target.value,
+          }))
+        }
+      />
+      <span
+        className='err'
+        style={{ visibility: `${error ? 'visible' : 'hidden'}` }}
+      >
+        {error}
+      </span>
+      <button className='btn' onClick={send}>
+        Crear personaje
+      </button>
     </div>
   )
 }
