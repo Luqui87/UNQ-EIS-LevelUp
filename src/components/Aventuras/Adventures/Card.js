19b61b6b38a79783e6e1940816a7fda55d7b99d6
Ivana Provvidenza <vanndere@gmail.com>
2023-05-21 14:04:31 -0300

    Agrego likes en aventura

--- src/components/Aventuras/Adventures/Card.js
+++ src/components/Aventuras/Adventures/Card.js
@@ -1,9 +1,43 @@
 import { Link } from 'react-router-dom'
-import { getFile } from './functions'
+import { getFile, getLikes } from './functions'
+import './votacion.css'
+import React, { useState, useReducer } from 'react'
+
+const appReducer = (state, action) => {
+  switch(action.type) {
+    case 'HANDLE_LIKE':
+      return {
+        ...state,
+        likes: state.likes + action.payload
+      }
+    default:
+      return state
+  }
+}
 
 export const Card = ({ aventura }) => {
   const file = getFile(aventura.pdf)
   localStorage.setItem('pdf', aventura.pdf)
+
+  const [state, dispatch] = useReducer(appReducer, aventura.likes)
+  const likes = state
+  const [status, setStatus] = useState(null)
+  
+  const handleClickLike = () => {
+    if (status==='like') {
+      setStatus(null)
+      dispatch({
+        type: 'HANDLE_LIKE',
+        payload: -1,
+      })
+    } else {
+      setStatus('like')
+      dispatch({
+        type: 'HANDLE_LIKE',
+        payload: 1,
+      })
+    }
+  }
 
   return (
     <li className='card'>
@@ -14,6 +48,10 @@ export const Card = ({ aventura }) => {
       <p>Nivel: {aventura.level}</p>
       <p>Duración: {aventura.duration}</p>
       <p>Idioma: {aventura.language}</p>
+          <button className={status==='like'? 'btn active' : 'likes'} onClick={handleClickLike}> 
+          Votar 
+            <span>{likes}</span>
+          </button>
       <span>
         <a href={aventura.pdf} download={`${aventura.title}`}>
           <button className='button-red' disabled={!file ? false : true}>
