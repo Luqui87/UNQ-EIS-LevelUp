bd8536aef0cb95bd8715ac262b2bc0db19912375
Luqui87 <alvarezlucas2787@gmail.com>
2023-06-07 19:16:42 -0300

    Refactor mantener despegable al iniciar sesión

--- src/components/NavBar/Menu.js
+++ src/components/NavBar/Menu.js
@@ -1,16 +1,53 @@
 import question from '../../resources/question_mark.jpg'
 import IniciarSesion from '../../components/Cuenta/IniciarSesion'
 import { Link } from 'react-router-dom'
-import { useContext } from 'react'
+import { useContext, useState, useEffect, useRef } from 'react'
 import { AuthContext } from '../AuthContext'
 import './menu.css'
 
+
+
 export const UnloggedMenu = () => {
+
+  const [visibility,setVisibility] = useState("hidden")
+
+  function useOutsideAlerter(ref) {
+    useEffect(() => {
+      /**
+       * Alert if clicked on outside of element
+       */
+      function handleClickOutside(event) {
+        if (ref.current && !ref.current.contains(event.target) ) {
+          setVisibility("hidden");
+        }
+      }
+      // Bind the event listener
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        // Unbind the event listener on clean up
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, [ref]);
+  } 
+  const wrapperRef = useRef(null)
+  useOutsideAlerter(wrapperRef)
+
+  const handleClickVisibility = () =>{
+    if (visibility == "hidden"){
+      setVisibility("visible")
+    }
+    else{
+      setVisibility("hidden")
+    }
+  }
+
   return (
-    <div className='menu'>
-      <img src={question} alt='perfil' />
-      <span>Invitado</span>
-      <div>
+    <div className='menu' ref={wrapperRef}>
+      <div className='InNav' onClick={() => handleClickVisibility()} >
+        <img src={question} alt='perfil' />
+        <span>Invitado</span>
+      </div>
+      <div className='despegable' style={{visibility: visibility}}>
         <IniciarSesion />
         <br />
         <Link to='/registro'>Registrar Cuenta</Link>
@@ -22,11 +59,45 @@ export const UnloggedMenu = () => {
 export const LoggedMenu = () => {
   const { setToken, username, setUsername } = useContext(AuthContext)
 
+  const [visibility,setVisibility] = useState("hidden")
+
+  function useOutsideAlerter(ref) {
+    useEffect(() => {
+      /**
+       * Alert if clicked on outside of element
+       */
+      function handleClickOutside(event) {
+        if (ref.current && !ref.current.contains(event.target) ) {
+          setVisibility("hidden");
+        }
+      }
+      // Bind the event listener
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        // Unbind the event listener on clean up
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, [ref]);
+  } 
+  const wrapperRef = useRef(null)
+  useOutsideAlerter(wrapperRef)
+
+  const handleClickVisibility = () =>{
+    if (visibility == "hidden"){
+      setVisibility("visible")
+    }
+    else{
+      setVisibility("hidden")
+    }
+  }
+  
   return (
-    <div className='menu'>
-      <img src={question} alt='perfil' />
-      <span>{username}</span>
-      <div>
+    <div className='menu' ref={wrapperRef}>
+       <div className='InNav' onClick={() => handleClickVisibility()} >
+        <img src={question} alt='perfil' />
+        <span>{username}</span>
+      </div>
+      <div className='despegable' style={{visibility: visibility}}>
         <button
           className='btnCerrarSesion'
           onClick={() => {
