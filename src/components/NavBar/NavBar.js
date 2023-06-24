aa4f512fd203a10c80486578d42f4314bb579074
Matias Aduco <matias.aduco@gmail.com>
2023-06-24 15:28:57 -0300

    Implementada la funcionalidad responsive de la navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,12 +1,27 @@
 import './navbar.css'
+import './navbar_responsive.css'
 import { Link } from 'react-router-dom'
 import { UnloggedMenu, LoggedMenu } from './Menu'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { AuthContext } from '../AuthContext.js'
 
 export const NavBar = ({ buttons }) => {
   const style = { color: 'white', textDecoration: 'none' }
   const { token } = useContext(AuthContext)
+  const [active, setActive] = useState('')
+
+  const showNavMenu = () => {
+    active ? setActive('') : setActive('active')
+    // const menu = document.getElementById('navmenu')
+    // const burger = document.getElementById('burger')
+    // menu.style.visibility === 'hidden'
+    //   ? (menu.style.visibility = 'visible')
+    //   : (menu.style.visibility = 'hidden')
+
+    // burger.style.visibility === 'hidden'
+    //   ? (menu.style.visibility = 'visible')
+    //   : (menu.style.visibility = 'hidden')
+  }
 
   return (
     <header>
@@ -14,15 +29,22 @@ export const NavBar = ({ buttons }) => {
         <Link to={'/'} className='logo'>
           <img src='dnd_logo.webp' alt='Logo DnD' />
         </Link>
-        <ul>
-          {buttons?.map((button, index) => (
-            <li key={`${index}-${button}`}>
-              <Link to={`/${button.toLowerCase()}`} style={style}>
-                {button}
-              </Link>
-            </li>
-          ))}
-        </ul>
+        <div>
+          <button
+            className={`burger ${active}`}
+            id='burger'
+            onClick={() => showNavMenu()}
+          />
+          <ul class={`navmenu ${active}`}>
+            {buttons?.map((button, index) => (
+              <li key={`${index}-${button}`}>
+                <Link to={`/${button.toLowerCase()}`} style={style}>
+                  {button}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
         {token ? <LoggedMenu /> : <UnloggedMenu />}
       </nav>
     </header>
